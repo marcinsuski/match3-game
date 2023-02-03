5745d00975e9c8608d07135532dd49e213da266d
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-02-03 16:38:26 +0100

    set board

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const App = () => {
+    const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+    const checkForColumnOfFour = () => {
+        for (let i = 0; i < 39; i++) {
+            const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+            const decidedColor = currentColorArrangement[i];
+
+            if (
+                columnOfFour.every(
+                    (square) => currentColorArrangement[square] === decidedColor
+                )
+            ) {
+                columnOfFour.forEach(
+                    (square) => (currentColorArrangement[square] = "")
+                );
+            }
+        }
+    };
+
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i < 47; i++) {
+            const columnOfThree = [i, i + width, i + width * 2];
+            const decidedColor = currentColorArrangement[i];
+
+            if (
+                columnOfThree.every(
+                    (square) => currentColorArrangement[square] === decidedColor
+                )
+            ) {
+                columnOfThree.forEach(
+                    (square) => (currentColorArrangement[square] = "")
+                );
+            }
+        }
+    };
+
+    const createBoard = () => {
+        const randomColorArrangement = [];
+        for (let i = 0; i < width * width; i++) {
+            const randomNumber = Math.floor(Math.random() * candyColors.length);
+            const randomColor = candyColors[randomNumber];
+            randomColorArrangement.push(randomColor);
+        }
+        setCurrentColorArrangement(randomColorArrangement);
+    };
+
+    useEffect(() => {
+        createBoard();
+    }, []);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfFour();
+            checkForColumnOfThree();
+            setCurrentColorArrangement([...currentColorArrangement]);
+        }, 100);
+        return () => clearInterval(timer);
+    }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
+
+    console.log(currentColorArrangement);
+
+    return (
+        <div className="app">
+            <div className="game">
+                {currentColorArrangement.map((candyColor, index) => (
+                    <img
+                        key={index}
+                        style={{ backgroundColor: candyColor }}
+                        src=""
+                        alt={candyColor}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
